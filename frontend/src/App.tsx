a6d95e099c0798d1745b66cd002036ccb1ab4b6e
Daniel Sanches <dh.sanchesp@gmail.com>
2023-10-19 00:36:36 -0300

    Consuming Users API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
+import { gql, useQuery } from "@apollo/client"
+import { NewUserForm } from "./components/NewUserForm"
+
+type User = {
+  id: string,
+  name: string
+}
+
+export const GET_USERS = gql`
+  query {
+    users {
+      id
+      name
+    }
+  }
+`
 
 function App() {
+  const { data } = useQuery<{ users: User[] }>(GET_USERS)
 
   return (
-    <h1>Hello world</h1>
+    <div>
+      <NewUserForm />
+      <ul>
+        {data?.users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </div>
   )
 }
 
